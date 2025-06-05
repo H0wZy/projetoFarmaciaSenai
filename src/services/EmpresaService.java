1a592b12dd01d9134f1aad9e2744cffb223c21f7
RaulFerna <raulfernadnesnetto@gamil.com>
2025-06-05 15:08:27 -0300

    raul

--- src/services/EmpresaService.java
+++ src/services/EmpresaService.java
@@ -1,5 +1,45 @@
 package services;
 
-public class EmpresaService {
-    
+import models.Empresa;
+import models.Produto;
+import models.Setor;
+import models.Transportadora;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class EmpresaService extends Empresa {
+    private Map<Integer, Map<Integer, Map<Produto, Integer>>> vendasMes;
+
+    public EmpresaService(String nome, List<Setor> setores, List<Produto> produtos, List<Transportadora> transportadoras) {
+        super(nome, setores, produtos, transportadoras);
+        this.vendasMes = new HashMap<>();
+    }
+
+    public void programarVenda(int ano, int mes, Produto produto, int quantidade) {
+        vendasMes.computeIfAbsent(ano, y  -> new HashMap<>()).computeIfAbsent(mes, x -> new HashMap<>()).merge(produto, quantidade , Integer::sum);
+    }
+
+
+
+    public int getMes(){
+        return LocalDate.now().getMonthValue();
+    } //get para o mês do computador
+
+
+    public int getAno(){
+        return LocalDate.now().getYear();
+    } //get para o ano do computador
+
+
+    public Map<Integer, Map<Integer, Map<Produto, Integer>>> getVendasMes() {
+        return vendasMes;
+    }// Pega as chaves de mes dia e quantidade de produto
+
+    public void setVendasMes(Map<Integer, Map<Integer, Map<Produto, Integer>>> vendasMes) {
+        this.vendasMes = vendasMes;
+    }
 }
